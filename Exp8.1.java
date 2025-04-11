01a3e040d8c04fbdcb79f3b7f10b1076afa088ea
invitiucs <139709152+invitiucs@users.noreply.github.com>
2025-04-11 11:30:16 +0530

    Update Exp8.1.java

--- Exp8.1.java
+++ Exp8.1.java
@@ -19,3 +19,41 @@ It checks the credentials.
 - Compile and deploy the servlet in Tomcat.
 Access the form in your browser:
 http://localhost:8080/login.html
+code
+  <!DOCTYPE html>
+<html>
+<head>
+<title>Login</title>
+</head>
+  <body>
+<form action="LoginServlet" method="post">
+Username: <input type="text" name="username" required><br>
+Password: <input type="password" name="password" required><br>
+<input type="submit" value="Login">
+</form>
+</body>
+</html>
+1. Login Servlet:
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+protected void doPost(HttpServletRequest request, HttpServletResponse response)
+throws ServletException, IOException {
+response.setContentType("text/html");
+PrintWriter out = response.getWriter();
+String username = request.getParameter("username");
+String password = request.getParameter("password");
+if ("admin".equals(username) && "admin123".equals(password)) {
+out.println("<h2>Welcome, " + username + "!</h2>");
+} else {
+out.println("<h2>Invalid Credentials. Try Again.</h2>");
+}
+out.close();
+}
+}
